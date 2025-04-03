8bdab5231cf39ef937853e0282a992107c5b5e7c
mohammad-nadr <amoo.nadr81@gmail.com>
2025-04-03 17:02:11 +0330

    text init

--- src/views/StarterPage.vue
+++ src/views/StarterPage.vue
@@ -22,14 +22,9 @@ const breadcrumbs = shallowRef([
   <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
   <v-row>
     <v-col cols="12" md="12">
-      <v-card title="Simple Title" >
+      <v-card :title="$t('project title')" >
         <v-card-text>
-
-          Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-          minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-          reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
-          officiate descent molls anim id est labours.
-
+          <p v-html="$t('Init Description')"></p>
         </v-card-text>
       </v-card>
     </v-col>
