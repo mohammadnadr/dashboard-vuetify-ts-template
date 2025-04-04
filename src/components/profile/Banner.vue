89df3eb37c637b56b63688513ee1445368dd6bf8
mohammad-nadr <amoo.nadr81@gmail.com>
2025-04-04 21:02:12 +0330

    about me description

--- src/components/profile/Banner.vue
+++ src/components/profile/Banner.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
-import { ref, shallowRef } from 'vue';
+import { ref, defineProps, defineEmits, computed } from 'vue';
 import profileBg from '@/assets/images/profile/Banana.jpeg';
 import UserImage from '@/assets/images/profile/avatar.jpeg';
 
-// icons
-import { PlusOutlined, ProfileOutlined, TeamOutlined, SolutionOutlined, FileImageOutlined, UserAddOutlined } from '@ant-design/icons-vue';
+// تعریف props
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required:true
+  }
+});
 
-const tab = ref(null);
-const items = shallowRef([
+// تعریف emits
+const emit = defineEmits(['update:modelValue']);
+const tab = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  }
+});
+
+// icons
+import {
+  PlusOutlined,
+  ProfileOutlined,
+  TeamOutlined,
+  SolutionOutlined,
+  FileImageOutlined,
+  UserAddOutlined
+} from '@ant-design/icons-vue';
+
+const items = ref([
   { tab: 'Profile', icon: ProfileOutlined, href: '' },
-  { tab: 'Followers', icon: TeamOutlined, href: '' },
-  { tab: 'Friends', icon: SolutionOutlined, href: '' },
-  { tab: 'Gallery', icon: FileImageOutlined, href: '' },
-  { tab: 'Friend Request', icon: UserAddOutlined, href: '' },
+  { tab: 'followers', icon: TeamOutlined, href: '' },
+  { tab: 'friends', icon: SolutionOutlined, href: '' },
+  { tab: 'gallery', icon: FileImageOutlined, href: '' },
+  { tab: 'friend_request', icon: UserAddOutlined, href: '' },
 ]);
 </script>
 
@@ -22,30 +47,32 @@ const items = shallowRef([
       <img :src="profileBg" alt="profile" class="w-100" />
       <v-row class="mt-4">
         <v-col lg="3" md="3" cols="12" class="text-center text-md-end">
-          <div class="userImage"><img :src="UserImage" width="150" alt="profile" class="rounded-md" /></div
-          ></v-col>
+          <div class="userImage w-md-75"><img :src="UserImage" width="150" alt="profile" class="rounded-md" /></div
+          >
+        </v-col>
         <v-col lg="9" md="9" cols="12">
           <v-row class="me-0">
             <v-col md="5" cols="12" class="text-center text-md-start">
-              <h5 class="text-h5">Mohammad Nadr</h5>
+              <h4 class="text-h4">Mohammad Nadr</h4>
               <small class="opacity-50">Web Developer</small>
             </v-col>
             <v-col md="7" cols="12">
               <div class="text-center text-md-end">
-                <v-btn color="secondary" variant="outlined" class="me-2">Message</v-btn>
-                <v-btn color="primary" variant="flat">
-                  <template v-slot:prepend>
-                    <PlusOutlined />
-                  </template>
-                  Send Request
+                <v-btn color="secondary"
+                       variant="outlined"
+                       class="me-2"
+                       :append-icon="PlusOutlined"
+                       href="https://www.linkedin.com/in/mohammad-nadr"
+                >
+                  Message
                 </v-btn>
               </div>
             </v-col>
             <v-col md="12">
               <v-tabs v-model="tab" color="primary" class="profiletab" scrollable>
                 <v-tab v-for="item in items" :key="item.tab" :to="item.href">
-                  <component :is="item.icon" :style="{ fontSize: '18px' }" class="me-2"></component>
-                  {{ item.tab }}
+                  <component :is="item.icon" class="me-2 text-subtitle-1"></component>
+                  {{ $t(item.tab) }}
                 </v-tab>
               </v-tabs>
             </v-col>
@@ -58,21 +85,21 @@ const items = shallowRef([
 <style lang="scss">
 .userImage {
   margin: -70px 0px 0px auto;
+
   [dir='rtl'] & {
     margin: -70px auto 0px 0px;
   }
 }
+
 .profiletab .v-slide-group__content {
   justify-content: end;
-  .v-btn--variant-text .v-btn__overlay {
-    background: transparent;
-  }
+
 }
 
 @media (max-width: 600px) {
   .userImage img {
-    width: 72px;
-    height: 72px;
+    width: 100px;
+    height: 100px;
   }
 }
 </style>
--- src/views/pages/aboutMe/AboutMe.vue
+++ src/views/pages/aboutMe/AboutMe.vue
@@ -11,13 +11,30 @@ const breadcrumbs = shallowRef([
     href: ''
   }
 ]);
+const tab = ref('Profile');
+
 </script>
 
 <template>
   <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
   <v-row>
     <v-col>
-      <Banner />
+      <Banner v-model="tab" />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item v-for="i in 5"
+                            :key="i"
+        >
+          <v-card flat>
+            <v-card-text>
+              {{ $t('about me description') }}
+            </v-card-text>
+          </v-card>
+        </v-tabs-window-item>
+      </v-tabs-window>
     </v-col>
   </v-row>
 </template>
